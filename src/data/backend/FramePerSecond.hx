7cb0748c27a1db295894fc03ed12c754aa9974f6
ACoolioDude <stefanbeta2008@gmail.com>
2026-01-27 15:34:25 +0100

    Updating AFC.png and FPS.hx

--- src/data/backend/FramePerSecond.hx
+++ src/data/backend/FramePerSecond.hx
@@ -351,7 +351,7 @@ class FramePerSecond extends Sprite {
             trace('Unable to grab GPU Name: $e');
         }
 
-        return 'Sbinator: ${EngineConfiguration.gameVersion}\nOS: ${osName}\nCPU: ${cpuName} - ${cpuArch}\nGPU: ${gpuName}\nBranch: ${Main.releaseCycle} - (Commit v${GitHub.getGitCommitHash()} - ${GitHub.getGitBranch()})';
+        return 'Sbinator v${EngineConfiguration.gameVersion}\nOS: ${osName}\nCPU: ${cpuName} - ${cpuArch}\nGPU: ${gpuName}\nBranch: ${Main.releaseCycle} - (Commit v${GitHub.getGitCommitHash()} - ${GitHub.getGitBranch()})';
     }
 
     function onKeyRelease(event:KeyboardEvent):Void {
